9729b1927707d1cdbdee4391933d48b2632e265c
andy hunt <andyhunt200@hotmail.com>
2016-05-21 19:43:38 +0100

    React component

--- hello/public/app.jsx
+++ hello/public/app.jsx
@@ -1,4 +1,28 @@
-    ReactDOM.render(
-      <h1>Hello React!</h1>,
-      document.getElementById('app')
-    );
+//most common react method
+var Greeter = React.createClass({
+  getDefaultProps: function (){
+    return {
+      name: 'Melv',
+      message: 'Hello world!'
+    };
+  },
+  render: function (){
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
+      );
+  }
+});
+
+var firstName = "Andy";
+var firstMessage= "This is from the react component";
+
+ReactDOM.render(
+  <Greeter name={firstName} />, 
+    document.getElementById('app')
+);
